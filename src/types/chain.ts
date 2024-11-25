8943635313e5cde31c1709d13ab5ec7287801352
I Putu Saputrayana <ikhyan7@gmail.com>
2024-11-25 21:10:12 +0800

    chore: add testnet network

--- src/types/chain.ts
+++ src/types/chain.ts
@@ -13,4 +13,17 @@ export enum Network {
   LISK = 'lisk',
   BASE = 'base',
   KAIA = 'kaia',
+
+  // TESTNETS
+  ETHEREUM_TESTNET = 'ethereum-testnet',
+  BINANCE_SMART_CHAIN_TESTNET = 'binance-smart-chain-testnet',
+  POLYGON_POS_TESTNET = 'polygon-pos-testnet',
+  ETHEREUM_HOLESKY = 'ethereum-holesky',
+  ETHEREUM_SEPOLIA = 'ethereum-sepolia',
+  LISK_SEPOLIA = 'lisk-sepolia',
+  STORY_TESTNET = 'story-testnet',
+  SKALE_CALYPSO_TESTNET = 'skale-calypso-testnet',
+  SKALE_EUROPA_TESTNET = 'skale-europa-testnet',
+  SKALE_NEBULA_TESTNET = 'skale-nebula-testnet',
+  SKALE_TITAN_TESTNET = 'skale-titan-testnet',
 }
